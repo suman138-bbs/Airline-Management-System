b7196cbefde2869396fa8b173e11e157f6fba04a
STSumanBhandari <suman.bhandari@shridharatechnologies.in>
2023-11-07 16:34:49 +0530

    Inital SetUp

---
+++ FlightsAndSearchService/README.md
@@ -0,0 +1,13 @@
+# Airline-Booking
+
+## Why we use src folder :
+
+- Because it modular and all the logic are put inside the src folder
+
+# ORM :Object relation Mapper
+
+Sequelize is ORM for RDBMS and mysql2 is use for connectecting to MySql Server
+
+- npx sequelize init
+
+- npx sequelize db:create
---
+++ FlightsAndSearchService/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import { PORT } from "./config/serverConfig.js";
+
+const setupAndStartServer = async () => {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+  });
+};
+
+setupAndStartServer();
